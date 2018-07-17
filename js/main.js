877409589bc89cecddcc8c54e22277a4dbc82c12
Sophie Popow <sophie.popow@gmail.com>
2018-07-17 23:39:08 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+console.log("Js dziala!");
+
+// var button = document.getElementsByClassName('jsMenuButton');
+
+var button = document.querySelector('.jsMenuButton');
+var menu = document.querySelector('.jsMenu');
+
+button.addEventListener('click', function(){
+  console.log('klikniecie');
+  menu.classList.toggle('menu-open');
+})
+console.log(button);
